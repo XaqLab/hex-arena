212b19a3e23a48571d9a8a0ce90685312d91a424
Zhe Li <zhe.li@bcm.edu>
2023-03-29 23:04:18 -0500

    Divide into smaller tests

--- tests/test_arena.py
+++ tests/test_arena.py
@@ -3,26 +3,36 @@ import unittest
 import numpy as np
 from hexarena.arena import Arena
 
+rng = np.random.default_rng()
+
 
 class TestArena(unittest.TestCase):
 
     def test_basics(self):
-        rng = np.random.default_rng()
         with self.assertRaises(Exception):
             arena = Arena(1)
         for resol in [2, 4, 6]:
             arena = Arena(resol=resol)
 
-            for x, y in [(0.5, 0), (0, 0.85)]:
-                self.assertTrue(arena.is_inside((x, y)))
-            for x, y in [(0.5**0.5, 0.5**0.5), (0, -0.87)]:
-                self.assertFalse(arena.is_inside((x, y)))
+    def test_is_inside(self):
+        arena = Arena()
+        for x, y in [(0.5, 0), (0, 0.85)]:
+            self.assertTrue(arena.is_inside((x, y)))
+        for x, y in [(0.5**0.5, 0.5**0.5), (0, -0.87)]:
+            self.assertFalse(arena.is_inside((x, y)))
 
-            r = rng.uniform(0, 3**0.5/2)
+    def test_get_state_index(self):
+        arena = Arena()
+        for _ in range(5):
+            r = rng.uniform(0, 1)
             theta = rng.uniform(0, 2*np.pi)
             x, y = r*np.cos(theta), r*np.sin(theta)
-            s_idx = arena.get_state_index((x, y))
-            self.assertLess(s_idx, arena.num_states)
+            if arena.is_inside((x, y)):
+                s_idx = arena.get_state_index((x, y))
+                self.assertLess(s_idx, arena.num_states)
+            else:
+                with self.assertRaises(Exception):
+                    arena.get_state_index((x, y))
 
 
 if __name__=='__main__':
