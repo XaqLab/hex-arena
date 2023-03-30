8d7aa4977a6ca90bef964e93394269bd1e106b5f
Zhe Li <zhe.li@bcm.edu>
2023-03-29 21:59:43 -0500

    Add method to check if a position is inside

--- hexarena/arena.py
+++ hexarena/arena.py
@@ -24,30 +24,30 @@ class Arena:
             anchors: six at the corner and one at the center.
 
         """
-        assert resol%2==1, (
-            "'resol' needs to be an odd number to allocate anchors on the center of walls."
+        assert resol%2==0, (
+            "'resol' needs to be an even number to allocate anchors on the center of walls."
         )
         self.resol = resol
 
         self.num_states = 3*self.resol**2+3*self.resol+1
         anchors = []
         self.corner_idxs, self.box_idxs = [], []
-        for i in range(self.resol):
+        for i in range(self.resol+1):
             if i==0:
                 anchors.append([0, 0])
                 continue
-            r = i/(self.resol-1)
+            r = i/self.resol
             for j in range(6):
                 theta = j/6*(2*np.pi)
                 x0, y0 = r*np.cos(theta), r*np.sin(theta)
                 theta += 2*np.pi/3
                 for k in range(i):
-                    if i==self.resol-1 and k==0:
+                    if i==self.resol and k==0:
                         self.corner_idxs.append(len(anchors))
-                    if i==self.resol-1 and j%2==1 and k==self.resol//2:
+                    if i==self.resol and j%2==1 and k==self.resol//2:
                         self.box_idxs.append(len(anchors))
-                    x = x0+k/(self.resol-1)*np.cos(theta)
-                    y = y0+k/(self.resol-1)*np.sin(theta)
+                    x = x0+k/self.resol*np.cos(theta)
+                    y = y0+k/self.resol*np.sin(theta)
                     anchors.append([x, y])
         self.anchors = np.array(anchors)
 
@@ -73,18 +73,18 @@ class Arena:
                 x+s_len/3**0.5*np.cos([theta-np.pi/6, theta+np.pi/6]),
                 y+s_len/3**0.5*np.sin([theta-np.pi/6, theta+np.pi/6]),
             ]))
-        s_len, segments = 1/(self.resol-1), []
+        s_len, segments = 1/self.resol, []
         for j in range(6):
             theta = j/6*(2*np.pi)
-            for i in range(self.resol):
+            for i in range(self.resol+1):
                 if i==0:
                     append_seg(0, 0, theta)
                 else:
-                    r = i/(self.resol-1)
+                    r = i/self.resol
                     x0, y0 = r*np.cos(theta), r*np.sin(theta)
                     for k in range(i):
-                        x = x0+k/(self.resol-1)*np.cos(theta+2*np.pi/3)
-                        y = y0+k/(self.resol-1)*np.sin(theta+2*np.pi/3)
+                        x = x0+k/self.resol*np.cos(theta+2*np.pi/3)
+                        y = y0+k/self.resol*np.sin(theta+2*np.pi/3)
                         for l in range(-1 if k==0 else 0, 3):
                             append_seg(x, y, theta+l*np.pi/3)
         for segment in segments:
@@ -96,9 +96,25 @@ class Arena:
         ax.set_yticks([-1, 1])
         ax.set_aspect('equal')
 
+    def is_inside(self,
+        pos: tuple[float, float],
+    ) -> bool:
+        r"""Returns if a position is inside the arena."""
+        x, y = pos
+        for i in range(6):
+            x0, y0 = self.anchors[self.corner_idxs[i]]
+            x1, y1 = self.anchors[self.corner_idxs[(i+1)%6]]
+            dx, dy = x-x0, y-y0
+            dx1, dy1 = x1-x0, y1-y0
+            if dx*dy1-dx1*dy>0:
+                return False
+        return True
+
     def get_state_index(self,
         pos: tuple[float, float],
     ) -> int:
+        r"""Get integer index of a position."""
+        assert self.is_inside(pos), f"Position {tuple(pos)} is outside the arena."
         d = ((np.array(pos)-self.anchors)**2).sum(axis=1)**0.5
         s_idx = np.argmin(d)
         return s_idx
--- tests/test_arena.py
+++ tests/test_arena.py
@@ -9,15 +9,20 @@ class TestArena(unittest.TestCase):
     def test_basics(self):
         rng = np.random.default_rng()
         with self.assertRaises(Exception):
-            arena = Arena(2)
-        for resol in [1, 3, 5]:
+            arena = Arena(1)
+        for resol in [2, 4, 6]:
             arena = Arena(resol=resol)
+
+            for x, y in [(0.5, 0), (0, 0.85)]:
+                self.assertTrue(arena.is_inside((x, y)))
+            for x, y in [(0.5**0.5, 0.5**0.5), (0, -0.87)]:
+                self.assertFalse(arena.is_inside((x, y)))
 
             r = rng.uniform(0, 3**0.5/2)
             theta = rng.uniform(0, 2*np.pi)
             x, y = r*np.cos(theta), r*np.sin(theta)
             s_idx = arena.get_state_index((x, y))
-            self.assertLess(s_idx, 3*resol**2+3*resol+1)
+            self.assertLess(s_idx, arena.num_states)
 
 
 if __name__=='__main__':
