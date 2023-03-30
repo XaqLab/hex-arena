1b1eac5bd044577ae987a4795784a380f5af82e4
Zhe Li <zhe.li@bcm.edu>
2023-03-29 20:22:05 -0500

    Add basic files for setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+with open('hexarena/VERSION.txt', 'r') as f:
+    VERSION = f.readline().split('"')[1]
+
+setup(
+    name="hexarena",
+    version=VERSION,
+    author='Zhe Li',
+    python_requires='>=3.9',
+    packages=find_packages(),
+    package_data={'hexarena': ['VERSION.txt']},
+    install_requires=['numpy', 'torch', 'pyyaml'],
+)
