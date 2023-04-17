e85ef96f2b43584037bd1fc0968e2409ba3457b8
Zhe Li <zhe.li@bcm.edu>
2023-04-17 13:07:00 -0500

    Minor update

--- train-one-agent.py
+++ train-one-agent.py
@@ -1,12 +1,18 @@
+from jarvis.config import Config, from_cli
 from irc.manager import AgentManager
 
+cli_args = Config({
+    'store_dir': 'store',
+    'defaults': 'irc-defaults.yaml',
+    'env_param': [1, 0.001, 0.1, 0, 0.0286, 0.2, 0.0476, 0.2, 0.0667, 0.2],
+})
+
 if __name__=='__main__':
+    cli_args.update(from_cli())
     manager = AgentManager(
-        'store', defaults='irc-defaults.yaml', eval_interval=1, disp_interval=1,
+        store_dir=cli_args.pop('store_dir'),
+        defaults=cli_args.pop('defaults'),
+        save_interval=3,
     )
 
-    config = {
-        'env_param': [1, 0.001, 0.1, 0, 0.0286, 0.2, 0.0476, 0.2, 0.0667, 0.2],
-    }
-    config = manager.get_config(config)
-    manager.process(config, num_epochs=18)
+    manager.train_one_agent(**cli_args)
