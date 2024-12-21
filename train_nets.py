dce70525d0a86916b6ad95d62a58666d98546f31
Zhe Li <zheli24@stanford.edu>
2024-12-20 18:03:45 -0600

    Rewrite train_nets.py to use Manager object

--- train_nets.py
+++ train_nets.py
@@ -1,94 +1,121 @@
-import os, pickle, torch
 from pathlib import Path
-from jarvis.config import from_cli, Config
-from jarvis.utils import tqdm
+from jarvis.config import from_cli, choices2configs, Config
+from jarvis.manager import Manager
+from jarvis.utils import tqdm, tensor2array, array2tensor
 
 from hexarena import DATA_DIR, STORE_DIR
 
 from compute_beliefs import prepare_blocks, create_model, fetch_beliefs
 
 
+def create_manager(
+    data_dir: Path, store_dir: Path, subject: str, kappa: float,
+    num_samples: int = 1000, patience: float = 12.,
+) -> Manager:
+    r"""Creates a manager to train networks for belief dynamics.
+
+    Args
+    ----
+    data_dir:
+        Data directory, see `get_data_path` for more details.
+    store_dir:
+        Directory of storing trained VAE models.
+    subject:
+        Subject name, see `get_data_path` for more details.
+    kappa:
+        Cue reliability, see `get_data_path` for more details.
+    num_samples:
+        Number of samples used in belief estimation.
+    patience:
+        Arguments of the Manager object, see `Manager` for more details.
+
+    """
+    manager = Manager(
+        store_dir=store_dir/'belief_nets'/subject, patience=patience,
+    )
+    block_ids = prepare_blocks(data_dir, subject, kappa, verbose=False)
+    manager.observations, manager.actions, _, manager.beliefs = zip(*[
+        fetch_beliefs(
+            data_dir, store_dir, subject, session_id, block_idx, num_samples,
+        ) for session_id, block_idx in tqdm(block_ids, unit='block', leave=False)
+    ])
+    manager.default = {
+        'seed': 0, 'model_kw': {},
+        'update_kw': {'z_reg': 1e-4, 'num_epochs': 300},
+        'init_kw': {'z_reg': 1e-4, 'num_epochs': 200},
+    }
+
+    def setup(config: Config):
+        r"""
+        config:
+          - subject: str
+          - kappa: float
+          - num_samples: int
+        """
+        manager.seed = config.seed
+        manager.model_kw = config.model_kw
+        manager.train_kw = {
+            'update_kw': config.update_kw, 'init_kw': config.init_kw,
+        }
+        return 1 # one epoch for manager
+    def reset():
+        _, manager.model = create_model(subject, kappa, model_kw=manager.model_kw)
+        manager.model.init_net.reset(manager.seed)
+        manager.model.update_net.reset(manager.seed)
+        manager.stats_u, manager.stats_i = None, None
+    def step():
+        manager.stats_u, manager.stats_i = manager.model.train_nets(
+            manager.observations, manager.actions, manager.beliefs,
+            strict_init=True, **manager.train_kw,
+        )
+    def get_ckpt():
+        return tensor2array({
+            'state_dict': manager.model.state_dict(),
+            'stats_u': manager.stats_u, 'stats_i': manager.stats_i,
+        })
+    def load_ckpt(ckpt):
+        ckpt = array2tensor(ckpt, manager.model.device)
+        manager.model.load_state_dict(ckpt['state_dict'])
+        manager.stats_u = ckpt['stats_u']
+        manager.stats_i = ckpt['stats_i']
+        return 1
+    manager.setup = setup
+    manager.reset = reset
+    manager.step = step
+    manager.get_ckpt = get_ckpt
+    manager.load_ckpt = load_ckpt
+    return manager
+
 def main(
-    data_dir: Path|str,
-    store_dir: Path|str,
-    subject: str,
-    num_samples: int,
-    seeds: list[int],
-    update_kw: dict|None = None,
-    init_kw: dict|None = None,
+    data_dir: Path|str, store_dir: Path|str,
+    subject: str, kappa: float, num_samples: int,
+    choices: dict|Path|str|None = None,
+    patience: float = 12., num_works: int|None = None,
 ):
     r"""Trains belief networks for a belief model.
 
     Args
     ----
-    data_dir:
-        Directory that contains experiment data file, e.g. 'data_marco.mat'.
-    store_dir:
-        Directory for storage, containing precomputed beliefs using sampling
-        methods, see `compute_beliefs.py` for more details.
-    subject:
-        Subject name.
-    num_samples:
-        Number of samples used in estimating beliefs, used to identify the saved
-        belief data.
+    data_dir, store_dir, subject, kappa, num_samples:
+        Arguments of `create_manager`.
+    choices:
+        Job specifications which is a dict containing possible values.
 
     """
-    data_dir = Path(data_dir)
-    store_dir = Path(store_dir)
-    update_kw = Config(update_kw).fill({
-        'z_reg': 1e-4, 'num_epochs': 300,
-    })
-    init_kw = Config(init_kw).fill({
-        'z_reg': 1e-4, 'num_epochs': 200,
-    })
-
-    # collect experiment data along with precomputed beliefs
-    block_ids = prepare_blocks(data_dir, subject)
-    observations, actions, beliefs = [], [], []
-    for session_id, block_idx in tqdm(block_ids, unit='block'):
-        _observations, _actions, _, _beliefs = fetch_beliefs(
-            data_dir, store_dir, subject, session_id, block_idx, num_samples,
-        )
-        observations.append(_observations)
-        actions.append(_actions)
-        beliefs.append(torch.tensor(_beliefs))
-
-    _, model = create_model(subject)
-    for seed in tqdm(seeds, unit='seed'):
-        save_pth = store_dir/'belief_nets/{}_[seed{:02d}].pkl'.format(subject, seed)
-        meta = Config({
-            'subject': subject, 'seed': seed,
-            'block_ids': block_ids, 'num_samples': num_samples,
-            'update_kw': update_kw, 'init_kw': init_kw,
+    data_dir, store_dir = Path(data_dir), Path(store_dir)
+    manager = create_manager(
+        data_dir, store_dir, subject, kappa, num_samples, patience,
+    )
+    if choices is None or isinstance(choices, dict):
+        choices = Config(choices).fill({
+            'seed': list(range(6)),
+            'model_kw.z_dim': [8, 16, 32],
         })
-
-        # check if the model instance is trained
-        to_skip = False
-        if os.path.exists(save_pth):
-            with open(save_pth, 'rb') as f:
-                saved = pickle.load(f)
-            to_skip = Config(saved['meta'])==meta
-        if to_skip:
-            continue
-
-        # train update_net and init_net of the belief model
-        model.init_net.reset(seed)
-        model.update_net.reset(seed)
-        stats_u, stats_i = model.train_nets(
-            observations, actions, beliefs, strict_init=True,
-            update_kw=update_kw, init_kw=init_kw,
-        )
-        os.makedirs(save_pth, exist_ok=True)
-        with open(save_pth, 'wb') as f:
-            pickle.dump({
-                'meta': meta.asdict(), 'state_dict': model.state_dict(),
-                'stats_u': stats_u, 'stats_i': stats_i,
-            }, f)
-
+    configs = choices2configs(choices)
+    manager.batch(configs, 1, num_works)
 
 if __name__=='__main__':
     main(**from_cli().fill({
         'data_dir': DATA_DIR, 'store_dir': STORE_DIR,
-        'subject': 'marco', 'num_samples': 1000,
-        'seeds': list(range(12)),
+        'subject': 'marco', 'kappa': 0.1, 'num_samples': 1000,
     }))
