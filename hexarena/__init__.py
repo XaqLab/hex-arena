90ce832acd70e05e89e8d3e84ef3d2cd72fd9d52
Zhe Li <zheli24@stanford.edu>
2025-01-15 15:08:42 -0800

    Minor update

--- hexarena/__init__.py
+++ hexarena/__init__.py
@@ -3,9 +3,14 @@ from pathlib import Path
 
 __version__ = '0.1.2'
 
-_DATA_DIRS = [
+_data_dirs = [
     Path(__file__).parent.parent/'data',
     Path('/mnt/c/Projects.Data/foraging/from.Panos_Jun2024'),
+    Path('/mnt/d/Projects.Data/foraging/from.Panos_Jun2024'),
 ]
-DATA_DIR = next((d for d in _DATA_DIRS if os.path.exists(d)), None)
-STORE_DIR = Path(__file__).parent.parent/'store'
+DATA_DIR = next((d for d in _data_dirs if os.path.exists(d)), None)
+_store_dirs = [
+    Path(__file__).parent.parent/'store',
+    Path('/mnt/d/foraging'),
+]
+STORE_DIR = next((d for d in _store_dirs if os.path.exists(d)), _store_dirs[0])
