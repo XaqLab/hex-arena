f9b79c2c17b230c9ecc6417101edc9cda4e106f6
Zhe Li <zheli24@stanford.edu>
2025-05-20 16:16:38 -0700

    Update script for agent training

--- hexarena/scripts/train_agents.py
+++ hexarena/scripts/train_agents.py
@@ -1,146 +1,17 @@
 
 from pathlib import Path
-from gymnasium.wrappers import TimeLimit
-from stable_baselines3 import PPO
-import numpy as np
 from jarvis.config import from_cli, choices2configs, Config
-from jarvis.manager import Manager
-from jarvis.utils import tqdm, get_defaults, tensor2array, array2tensor
-from irc.utils.train import exp_fit
+from irc.agent.train import create_manager
 
 from .. import STORE_DIR
-from ..env import ForagingEnv
-from ..monkey import Monkey
-from ..box import BaseFoodBox
-from ..utils import ProgressBarCallback
-from .train_nets import fetch_best_net
-
-
-TIME_LIMIT = 1200   # episode time limit in PPO
-NUM_STEPS = 2000    # number of time steps per update in PPO
-BATCH_SIZE = 100    # batch size in PPO
-NUM_UPDATES = 10    # number of policy updates per epoch
-
-
-def create_manager(
-    subject: str, kappa: float, num_samples: int,
-    save_interval: int = 1, patience: float = 1.,
-) -> Manager:
-    r"""Creates a manager for training RL agents.
-
-    Args
-    ----
-    subject, kappa, num_samples:
-        Subject name, cue reliability and number of state samples, see `main`
-        for more details.
-    save_interval, patience:
-        Arguments of the manager, see `Manager` for more details.
-
-    """
-    manager = Manager(
-        STORE_DIR/'agents'/subject, save_interval=save_interval, patience=patience,
-    )
-    manager.default = Config({'seed': 0, 'gamma': 0.99, 'ent_coef': 0.01})
-    manager.default.fill(get_defaults(ForagingEnv, ['time_cost']))
-    manager.default.fill(get_defaults(
-        Monkey, ['push_cost', 'turn_price', 'move_price', 'look_price', 'center_cost'],
-    ))
-    manager.default.fill(get_defaults(BaseFoodBox, ['reward']))
-
-    manager.default['model'], manager.model = fetch_best_net(subject, kappa, num_samples)
-    manager.env = manager.model.env
-    manager.model.enable_info = False
-    manager.model.keep_grad = False
-
-    def setup(config: Config) -> int:
-        r"""
-        config:
-          - seed: int           # seed for belief nets
-          - time_cost: float    # time cost
-          - push_cost: float    # push cost
-          - turn_price: float   # turn price, 1/deg
-          - move_price: float   # move price, 1/(1^2)
-          - look_price: float   # look price, 1/deg
-          - center_cost: float  # center cost
-          - reward: float       # box reward
-          - gamma: float        # reward decay
-          - ent_coef: float     # entropy coefficient
-
-        """
-        manager.env.time_cost = config.time_cost
-        manager.env.monkey.set_param([
-            config.push_cost, config.turn_price, config.move_price,
-            config.look_price, config.center_cost,
-        ])
-        for box in manager.env.boxes:
-            box.reward = config.reward
-        manager.algo = PPO(
-            env=TimeLimit(manager.model, TIME_LIMIT),
-            policy='MlpPolicy', device='cpu',
-            n_steps=NUM_STEPS, batch_size=BATCH_SIZE,
-            gamma=config.gamma, ent_coef=config.ent_coef,
-        )
-        return float('inf')
-    def reset():
-        manager.logs = []
-        manager.model.reset()
-    def step():
-        manager.algo.policy.set_training_mode(True)
-        with tqdm(total=NUM_UPDATES, unit='update', leave=False) as pbar:
-            pbar_cb = ProgressBarCallback(pbar, disp_freq=NUM_STEPS)
-            pbar_cb.logs = manager.logs
-            if len(manager.logs)>0:
-                pbar_cb.reward, pbar_cb.food = manager.logs[-1]
-            manager.algo.env.envs[0].needs_reset = False
-            manager.algo.learn(
-                total_timesteps=NUM_UPDATES*NUM_STEPS,
-                callback=pbar_cb, reset_num_timesteps=False,
-            )
-        manager.algo.policy.set_training_mode(False)
-    def get_ckpt():
-        if len(manager.logs)>=NUM_UPDATES:
-            rewards, _ = np.array(manager.logs).T
-            _, optimality, _ = exp_fit(
-                np.arange(len(rewards), dtype=float), rewards, ascending=True,
-            )
-        else:
-            optimality = np.nan
-        return tensor2array({
-            'policy': manager.algo.policy.state_dict(),
-            'logs': manager.logs,
-            'last_obs': manager.algo._last_obs,
-            'elapsed_steps': manager.algo.env.envs[0].env._elapsed_steps,
-            'env_state': manager.env.get_state(),
-            'known': manager.model.known,
-            'belief': manager.model.belief,
-            'optimality': optimality,
-        })
-    def load_ckpt(ckpt) -> int:
-        ckpt = array2tensor(ckpt, manager.algo.device)
-        manager.algo.policy.load_state_dict(ckpt['policy'])
-        manager.logs = ckpt['logs']
-        manager.algo._last_obs = ckpt['last_obs']
-        manager.algo.env.envs[0].env._elapsed_steps = ckpt['elapsed_steps']
-        manager.env.set_state(ckpt['env_state'])
-        manager.model.known = ckpt['known']
-        manager.model.belief = array2tensor(ckpt['belief'])
-        return len(manager.logs)//NUM_UPDATES
-
-    manager.setup = setup
-    manager.reset = reset
-    manager.step = step
-    manager.get_ckpt = get_ckpt
-    manager.load_ckpt = load_ckpt
-    return manager
 
 
 def main(
-    subject: str, kappa: float, num_samples: int,
+    reward: float = 10., kappa: float = 0.1, schedule: str = 'Gamma',
     choices: dict|Path|str|None = None,
-    num_epochs: int = 20,
+    num_epochs: int = 10,
     num_works: int|None = None,
     max_errors: int = 10,
-    **kwargs,
 ):
     r"""Trains RL agents for different reward parameters.
 
@@ -161,19 +32,31 @@ def main(
         Keyword arguments of the manager, see `create_manager` for more details.
 
     """
-    manager = create_manager(subject, kappa, num_samples, **kwargs)
+    env = {'_target_': 'hexarena.env.ForagingEnv'}
+    box_kw = {'reward': reward, 'kappa': kappa}
+    if schedule=='Expo':
+        box_kw.update({'_target_': 'hexarena.box.PoissonBox'})
+        taus = [35, 21, 15]
+    if schedule=='Gamma':
+        box_kw.update({'_target_': 'hexarena.box.GammaBox'})
+        taus = [21, 14, 7]
+    env['boxes'] = [{'tau': tau, **box_kw} for tau in taus]
     if choices is None or isinstance(choices, dict):
         choices = Config(choices).fill({
-            'seed': list(range(12)),
-            'push_cost': [0., 1., 2.],
-            'reward': [5., 10.],
-            'turn_price': [0.005],
-            'move_price': [0., 1.],
-            'center_cost': [0., 2., 4.],
-            'gamma': [0.99, 0.9],
-            'ent_coef': [0.1, 0.05],
+            'env.monkey': {
+                'push_cost': [0., 1., 2.],
+                'center_cost': [0., 2., 4.],
+            },
+            'algo': {
+                '_target_': ['sb3_contrib.RecurrentPPO', 'stable_baselines3.PPO'],
+                'gamma': [0.99, 0.9, 0.6],
+                'ent_coef': [0.1, 0.05, 0.01],
+            },
+            'learn.seed': list(range(8)),
         })
     configs = choices2configs(choices)
+
+    manager = create_manager(env, STORE_DIR/'agents/ppo')
     manager.batch(
         configs, num_epochs, num_works, max_errors,
         pbar_kw={'unit': 'agent'},
@@ -181,8 +64,4 @@ def main(
 
 
 if __name__=='__main__':
-    main(**from_cli().fill({
-        'subject': 'marco',
-        'kappa': 0.1,
-        'num_samples': 1000,
-    }))
+    main(**from_cli())
