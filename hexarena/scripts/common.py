826666dec32b946ede53e08feefbf3284793d924
Zhe Li <zheli24@stanford.edu>
2025-06-13 23:54:59 -0500

    Add function to create default environment

--- hexarena/scripts/common.py
+++ hexarena/scripts/common.py
@@ -5,8 +5,13 @@ from ..env import SimilarBoxForagingEnv
 from ..utils import get_valid_blocks
 
 
+GAMMAS = { # default shape parameter of schedule distribution
+    'marco': 1., 'viktor': 10.,
+}
+
 def get_block_ids(
     subject: str, kappa: float,
+    gamma: float|None = None,
     min_pos_ratio: float = 0.5,
     min_gaze_ratio: float = 0.1,
     **kwargs,
@@ -17,25 +22,77 @@ def get_block_ids(
     ----
     subject, kappa:
         Subject name and cue reliability.
+    gamma:
+        Shape parameter of reward interval distribution.
     min_pos_ratio, min_gaze_ratio:
         Minimum ratio value of position data and gaze data respectively.
     kwargs:
         Additional keyword arguments for `get_valid_blocks`.
 
+    Returns
+    -------
+    block_ids:
+        Sorted block IDs as `(session_id, block_idx)`.
+
     """
+    if gamma is None:
+        gamma = GAMMAS[subject]
     block_infos = get_valid_blocks(
         subject, min_pos_ratio=min_pos_ratio, min_gaze_ratio=min_gaze_ratio, **kwargs,
     )
     block_ids = []
     for (session_id, block_idx), meta in block_infos.items():
-        if meta['kappa']!=kappa:
-            continue
-        if subject=='marco' and meta['gamma']!=1:
-            continue
-        if subject=='viktor' and meta['gamma']!=10:
+        if meta['kappa']!=kappa or meta['gamma']!=gamma:
             continue
         block_ids.append((session_id, block_idx))
     return sorted(block_ids)
+
+
+def create_env(
+    gamma: float = 1., kappa: float = 0.1,
+    env_kw: dict|None = None,
+) -> SimilarBoxForagingEnv:
+    r"""Creates default foraging environment.
+
+    Args
+    ----
+    gamma:
+        Shape parameter of the Gamma distribution of reward interval. Only '1'
+        and '10' are supported. When `gamma==1`, the food box uses exponential
+        schedule with 'tau' as 35, 21 and 15 secs. When `gamma==10`, the food
+        boxes uses Gamma schedule and linear cue with 'tau' as 21, 14 and 7 secs.
+    kappa:
+        Cue reliability.
+    env_kw:
+        Additional keyword arguments of environment, see `SimilarBoxForagingEnv`
+        for more details.
+
+    Returns
+    -------
+    env:
+        A default environment with boxes sorted from worst to best.
+
+    """
+    assert gamma in [1, 10], "Unsupported gamma distribution shape"
+    assert kappa>0, "Cue reliability needs to be positive"
+    if gamma==1:
+        env_kw = Config(env_kw).fill({
+            'box': {
+                '_target_': 'hexarena.box.PoissonBox',
+                'kappa': kappa,
+            },
+            'boxes': [{'tau': tau} for tau in [35, 21, 15]],
+        })
+    if gamma==10:
+        env_kw = Config(env_kw).fill({
+            'box': {
+                '_target_': 'hexarena.box.GammaBox',
+                'kappa': kappa,
+            },
+            'boxes': [{'tau': tau} for tau in [21, 14, 7]],
+        })
+    env = SimilarBoxForagingEnv(**env_kw)
+    return env
 
 
 def create_env_and_model(
