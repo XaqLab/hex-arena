10147730e8745448def4e171719a34f01fe40ea5
Zhe Li <zheli24@stanford.edu>
2026-07-17 23:30:29 -0700

    Add temporary script to test IRC example

--- hexarena/scripts/irc_example.py
+++ hexarena/scripts/irc_example.py
@@ -2,14 +2,14 @@ import yaml
 from itertools import product
 from jarvis.config import from_cli
 from irc.manager import RationalPolicyManager
+from irc.example.env import FoodBoxEnv
 
 from .. import STORE_DIR
 from .common import create_env
 
 
 def main(
-    bandit: bool = True,
-    spec_pth: str = 'rational.spec.yaml',
+    spec_pth: str = 'irc.example.spec.yaml',
     n_works: int|None = None,
     **kwargs,
 ):
@@ -17,9 +17,6 @@ def main(
 
     Args
     ----
-    bandit:
-        Whether to train for bandit environment or not. If ``True``, bandit
-        environment with Poisson boxes is used.
     spec_pth:
         Path to the yaml file that specifies the parameter grid.
     n_works:
@@ -29,26 +26,22 @@ def main(
         see `RationalPolicyManager.setup` for more details.
 
     """
-    if not bandit:
-        raise NotImplementedError
-    env = create_env(gamma=(1 if bandit else 10), no_arena=bandit)
+    env = FoodBoxEnv()
     with open(STORE_DIR/spec_pth, 'r') as f:
         spec = yaml.safe_load(f)
     configs = []
-    for seed, gamma, ent_coef, push_cost, tau0, tau1, tau2 in product(
-        spec['seed'], spec['gamma'], spec['ent_coef'], spec['push_cost'],
-        spec['tau'], spec['tau'], spec['tau'],
+    for seed, gamma, ent_coef, p_appear, p_cue in product(
+        spec['seed'], spec['gamma'], spec['ent_coef'],
+        spec['p_appear'], spec['p_cue'],
     ):
-        env.monkey.push_cost = push_cost
-        env.boxes[0].tau = tau0
-        env.boxes[1].tau = tau1
-        env.boxes[2].tau = tau2
+        env.p_appear = p_appear
+        env.p_cue = p_cue
         configs.append({
             'param': env.get_param(), 'seed': seed,
             'gamma': gamma, 'ent_coef': ent_coef,
             **kwargs,
         })
-    manager = RationalPolicyManager(env, STORE_DIR/'rational')
+    manager = RationalPolicyManager(env, STORE_DIR/'rational/irc.example')
     manager.batch(configs, n_works=n_works)
 
 
