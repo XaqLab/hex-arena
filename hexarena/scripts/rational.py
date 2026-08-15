dc87d2e9e534c13cbd44e4aadffdba4e34cd67f5
Zhe Li <zheli24@stanford.edu>
2026-08-14 21:07:06 -0700

    Update script of getting rational policies
    Only 6 permutations of 3 boxes schedules are now tested.

--- hexarena/scripts/rational.py
+++ hexarena/scripts/rational.py
@@ -1,5 +1,5 @@
 import yaml
-from itertools import product
+from itertools import product, permutations
 from jarvis.config import from_cli
 from irc.manager import RationalPolicyManager
 
@@ -34,20 +34,21 @@ def main(
     env = create_env(gamma=(1 if bandit else 10), no_arena=bandit)
     with open(STORE_DIR/spec_pth, 'r') as f:
         spec = yaml.safe_load(f)
+    taus = spec.pop('taus', [15., 21., 35.])
+    assert len(taus)==env.n_boxes, f"Expect {env.n_boxes} different tau values."
     configs = []
-    for seed, gamma, ent_coef, push_cost, tau0, tau1, tau2 in product(
+    for seed, gamma, ent_coef, push_cost in product(
         spec['seed'], spec['gamma'], spec['ent_coef'], spec['push_cost'],
-        spec['tau'], spec['tau'], spec['tau'],
     ):
         env.monkey.push_cost = push_cost
-        env.boxes[0].tau = tau0
-        env.boxes[1].tau = tau1
-        env.boxes[2].tau = tau2
-        configs.append({
-            'param': env.get_param(), 'seed': seed,
-            'gamma': gamma, 'ent_coef': ent_coef,
-            **kwargs,
-        })
+        for perm in permutations(range(env.n_boxes)):
+            for i in range(env.n_boxes):
+                env.boxes[i].tau = taus[perm[i]]
+            configs.append({
+                'param': env.get_param(), 'seed': seed,
+                'gamma': gamma, 'ent_coef': ent_coef,
+                **kwargs,
+            })
     manager = RationalPolicyManager(env, STORE_DIR/'rational')
     manager.batch(configs, n_works=n_works)
 
